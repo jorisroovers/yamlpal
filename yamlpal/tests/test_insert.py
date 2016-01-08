16e74b964d52c921ced985281b850599157e8212
Joris Roovers <joris.roovers@gmail.com>
2016-01-08 13:29:47 +0100

    Newlines are automatically added to newly inserted content
    Before, the user had to specify a \n character. This is no longer
    required.
    
    This also fixes our failing unit tests.

--- yamlpal/tests/test_insert.py
+++ yamlpal/tests/test_insert.py
@@ -8,17 +8,17 @@ class InsertionTests(BaseTestCase):
         self.cli = CliRunner()
 
     def test_insert_after_date(self):
-        result = self.cli.invoke(cli.cli, ["insert", "date", "newkey: value\n", self.get_sample_path("sample1")])
+        result = self.cli.invoke(cli.cli, ["insert", "date", "newkey: value", self.get_sample_path("sample1")])
         self.assertEqual(result.output, self.get_expected("sample1-after-date"))
 
     def test_insert_after_int(self):
-        result = self.cli.invoke(cli.cli, ["insert", "invoice", "newkey: value\n", self.get_sample_path("sample1")])
+        result = self.cli.invoke(cli.cli, ["insert", "invoice", "newkey: value", self.get_sample_path("sample1")])
         self.assertEqual(result.output, self.get_expected("sample1-after-int"))
 
     def test_insert_after_float(self):
-        result = self.cli.invoke(cli.cli, ["insert", "tax", "newkey: value\n", self.get_sample_path("sample1")])
+        result = self.cli.invoke(cli.cli, ["insert", "tax", "newkey: value", self.get_sample_path("sample1")])
         self.assertEqual(result.output, self.get_expected("sample1-after-float"))
 
     def test_insert_after_string(self):
-        result = self.cli.invoke(cli.cli, ["insert", "title", "newkey: value\n", self.get_sample_path("sample1")])
+        result = self.cli.invoke(cli.cli, ["insert", "title", "newkey: value", self.get_sample_path("sample1")])
         self.assertEqual(result.output, self.get_expected("sample1-after-string"))
