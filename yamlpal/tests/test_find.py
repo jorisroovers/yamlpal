01a85c350adc063a5b291e72d63385aaf2e619ed
Joris Roovers <jroovers@cisco.com>
2016-06-24 14:16:01 +0200

    Initial support for end line numbers
    Yamlpal is now able to determine the line number on which an element
    ends. This is particularly useful for finding the compound elements like
    dictionaries and lists which are spread across multiple lines.
    
    This capability is required for future additions that will add better smarts
    around finding/inserting/modifying/deleting content inside of compound elements.

--- yamlpal/tests/test_find.py
+++ yamlpal/tests/test_find.py
@@ -22,9 +22,9 @@ class FindTests(BaseTestCase):
     def test_find_custom_format(self):
         sample_path = self.get_sample_path("sample1")
         result = self.cli.invoke(cli.cli, ["find", "bill-to/address/city",
-                                           "-F", "%{file} %{linenr} %{key} %{value}",
+                                           "-F", "%{file} %{linenr} %{linenr.end} %{key} %{value}",
                                            "-f", sample_path])
-        self.assertEqual(result.output, "%s 11 city Royal Oak\n" % sample_path)
+        self.assertEqual(result.output, "%s 11 11 city Royal Oak\n" % sample_path)
 
     def test_find_custom_format_literal(self):
         result = self.cli.invoke(cli.cli, ["find", "bill-to/address/city",
@@ -44,14 +44,47 @@ class FindTests(BaseTestCase):
 
     def test_find_dictionary(self):
         result = self.cli.invoke(cli.cli, ["find", "bill-to/address", "-f", self.get_sample_path("sample1")])
-        expected = '{\n    "city": "Royal Oak", \n    "state": "MI", \n    "postal": "48046", \n    ' + \
+        expected = 'address: {\n    "city": "Royal Oak", \n    "state": "MI", \n    "postal": "48046", \n    ' + \
                    '"lines": "458 Walkman Dr.\\nSuite #292\\n"\n}\n'
         self.assertEqual(result.output, expected)
 
     def test_find_list(self):
         result = self.cli.invoke(cli.cli, ["find", "product", "-f", self.get_sample_path("sample1")])
-        expected = '[\n    {\n        "sku": "BL394D", \n        "price": "450.0", \n        ' + \
+        expected = 'product: [\n    {\n        "sku": "BL394D", \n        "price": "450.0", \n        ' + \
                    '"description": "Basketball", \n        "quantity": "4"\n    }, \n    ' + \
                    '{\n        "sku": "BL4438H", \n        "price": "2392.0", \n        ' + \
                    '"description": "Super Hoop", \n        "quantity": "1"\n    }\n]\n'
         self.assertEqual(result.output, expected)
+
+    def test_find_linenumbers(self):
+        query_result_mapping = {
+            'invoice': "2-2\n",
+            'date': "3-3\n",
+            'bill-to': "5-13\n",
+            'bill-to/given': "5-5\n",
+            'bill-to/family': "6-6\n",
+            'bill-to/address': "8-13\n",
+            'bill-to/address/lines': "8-10\n",
+            'bill-to/address/city': "11-11\n",
+            'bill-to/address/state': "12-12\n",
+            'bill-to/address/postal': "13-13\n",
+            # 'ship-to': "14-14\n", # TODO(jroovers): searching for references is not yet supported by yamlpal
+            'product': "16-23\n",
+            'product[0]': "16-19\n",
+            'product[1]': "20-23\n",
+            'product[0]/sku': "16-16\n",
+            'product[0]/quantity': "17-17\n",
+            'product[0]/description': "18-18\n",
+            'product[0]/price': "19-19\n",
+            'labels': "25-27\n",
+            'tax': "28-28\n",
+            'title': "29-29\n",
+            'total': "30-30\n",
+            # 'comments': "30-33\n", # TODO(jroovers): we don't correctly parse text that starts with > yet
+        }
+        for query, expected in query_result_mapping.iteritems():
+            # Test line numbers
+            result = self.cli.invoke(cli.cli, ["find", query, "-F", "%{linenr}-%{linenr.end}",
+                                               "-f", self.get_sample_path("sample1")])
+
+            self.assertEqual(result.output, expected, "%s: %s!=%s" % (query, result.output, expected))
